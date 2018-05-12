6ffddcb3bf60c53f1012ba9587ba0a3668c9a0d4
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-05-12 12:20:51 +0300

    Add files via upload

--- EnigmaEx/AgentModule/src/AgentModule/AgentFactory.java
+++ EnigmaEx/AgentModule/src/AgentModule/AgentFactory.java
@@ -1,4 +1,13 @@
 package AgentModule;
 
 public class AgentFactory {
+
+
+    //this function create and return new agent, (maybe give him task?)
+    public static Agent createAgent(){ //TODO:implement
+        //....
+        //new Agent(EnigmaMachine machine, String code);
+        return null;
+    }
+
 }
