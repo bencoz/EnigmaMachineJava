bf67bf78f2281414548dc9b805bc034743d272f8
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-05-12 11:33:34 +0300

    Add files via upload

--- EnigmaEx/DecryptionManager/src/DecryptionManager/MissionFactory.java
+++ EnigmaEx/DecryptionManager/src/DecryptionManager/MissionFactory.java
@@ -2,7 +2,11 @@ package DecryptionManager;
 
 public class MissionFactory {
 
+    //create new mission
+    public DecipherMission createMission(Integet _size,Integer _taskSize, Integer _numOfAgents)
 
+
+    //????
     public static DecipherMission Factory(){
         return null;
     }
