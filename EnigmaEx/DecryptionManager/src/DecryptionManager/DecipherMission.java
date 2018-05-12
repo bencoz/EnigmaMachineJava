cf6bd8c85617820f7e09044645c2dde5e085be8e
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-05-12 11:30:04 +0300

    Add files via upload

--- EnigmaEx/DecryptionManager/src/DecryptionManager/DecipherMission.java
+++ EnigmaEx/DecryptionManager/src/DecryptionManager/DecipherMission.java
@@ -4,5 +4,13 @@ public class DecipherMission {
     private Integer size;
     private Integer taskSize; //will be log of Mission Size
     private Integer numOfAgents;
+    private List<AgentTask> tasks;
+    private Integer currentTask; //index
 
+
+    //gets all mission details, and divide into sub-tasks(saved in tasks)
+    public DecipherMission(Integet _size,Integer _taskSize, Integer _numOfAgents){}
+
+    //return the next Aagent tasks
+    public getNextTask(){}
 }
