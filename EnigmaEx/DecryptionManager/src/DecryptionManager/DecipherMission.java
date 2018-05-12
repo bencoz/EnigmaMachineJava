33d106a0caf78aa16d3ab4643d54f0e23f742dba
edenhartman <34005340+edenhartman@users.noreply.github.com>
2018-05-12 12:13:56 +0300

    Add files via upload

--- EnigmaEx/DecryptionManager/src/DecryptionManager/DecipherMission.java
+++ EnigmaEx/DecryptionManager/src/DecryptionManager/DecipherMission.java
@@ -4,5 +4,14 @@ public class DecipherMission {
     private Integer size;
     private Integer taskSize; //will be log of Mission Size
     private Integer numOfAgents;
+    private List<AgentTask> tasks;
+    private Integer currentTask; //index
+    private Secret firstPos;
 
+
+    //gets all mission details, and divide into sub-tasks(saved in tasks)
+    public DecipherMission(Integet _size,Integer _taskSize, Integer _numOfAgents, Secret firstPos){} //TODO:implement
+
+    //return the next Aagent tasks
+    public getNextTask(){} //TODO:implement
 }
--- EnigmaEx/DecryptionManager/src/DecryptionManager/MissionFactory.java
+++ EnigmaEx/DecryptionManager/src/DecryptionManager/MissionFactory.java
@@ -2,8 +2,7 @@ package DecryptionManager;
 
 public class MissionFactory {
 
+    //create new mission //TODO:implement
+    public static DecipherMission createMission(DifficultyLevel diffculty, Integet _size,Integer _taskSize, Integer _numOfAgents){}
 
-    public static DecipherMission Factory(){
-        return null;
-    }
 }
